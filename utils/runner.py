1be80c1f8ff80d4b81970aa2a31869360216fad2
이기원 <kaiser5072@gmail.com>
2019-01-10 12:24:32 +0900

    Initial commit

--- utils/runner.py
+++ utils/runner.py
@@ -192,20 +192,23 @@ def get_recall(ratingTest, preds, test_mask, n_recalls):
     # temp = np.zeros((16980, 5551))
     # temp[(ratingTest[:, 0], ratingTest[:, 1])] = 1
     # temp = np.transpose(ratingTest)
-    temp = ratingTest
+    temp      = np.asarray(ratingTest)
+    preds     = np.asarray(preds)
+    test_mask = np.asarray(test_mask)
+
     non_zero_idx = np.sum(temp, axis=1) != 0
 
-    pred_user_interest = preds[non_zero_idx, :]
+    pred_user_interest   = preds[non_zero_idx, :]
     target_user_interest = temp[non_zero_idx, :]
-    test_mask = test_mask[non_zero_idx, :]
+    test_mask            = test_mask[non_zero_idx, :]
 
     pred_user_interest = pred_user_interest * test_mask + (1 - test_mask) * (-100)
     pred_user_interest = get_order_array(pred_user_interest)
     pred_user_interest = pred_user_interest <= n_recalls
 
-    match_interest = pred_user_interest * target_user_interest
-    num_match = np.sum(match_interest, axis=1)
-    num_interest = np.sum(target_user_interest, axis=1)
+    match_interest  = pred_user_interest * target_user_interest
+    num_match       = np.sum(match_interest, axis=1)
+    num_interest    = np.sum(target_user_interest, axis=1)
 
     user_recall = num_match / num_interest
     recall = np.average(user_recall)
