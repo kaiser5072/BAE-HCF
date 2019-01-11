3b989837dee03751858a08e29f46b5539154b11b
이기원 <kaiser5072@gmail.com>
2019-01-12 01:05:06 +0900

    Initial commit]

--- utils/runner.py
+++ utils/runner.py
@@ -167,9 +167,8 @@ def predict(infer_func, params):
         preds, ratingTest, mask = [], [], []
         with tqdm.tqdm(total=height) as pbar:
             for pred in eval_result:
-                _pred = pred['preds'][:10000]
-                print(_pred)
-                _rating = pred['ratingTest'][:10000]
+                _pred = pred['preds'][:, :10000]
+                _rating = pred['ratingTest'][:, :10000]
 
                 preds.append(_pred)
                 ratingTest.append(_rating)
