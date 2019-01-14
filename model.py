2a51135b044232f9b0cd035ce25d8d38b5b883f7
이기원 <kaiser5072@gmail.com>
2019-01-14 17:27:05 +0900

    Initial commit

--- model.py
+++ model.py
@@ -83,7 +83,7 @@ class AE_CF(object):
         with tf.variable_scope('layer%d'%self.n_layer):
             w = tf.get_variable('weight', shape=[h.get_shape()[1], self.dims[-1]],
                                 trainable=True,
-                                initializer=w_init,
+                                initializer=tf.initializers.truncated_normal(stddev=0.01),
                                 dtype=self.dtype)
 
             self.outputs = tf.matmul(h, w)
