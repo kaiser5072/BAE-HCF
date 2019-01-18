3ccf31bd673e5db7923c76b13aa9f993d2595dae
이기원 <kaiser5072@gmail.com>
2019-01-19 02:08:40 +0900

    Initial commit

--- model.py
+++ model.py
@@ -57,7 +57,7 @@ class AE_CF(object):
                 h = tf.sparse.matmul(inputs, w) + b + tf.sparse.matmul(sides, s)
                 # else:
                 #     h = b + tf.sparse.matmul(sides, s)
-                # h = tf.layers.batch_normalization(h)
+                h = tf.layers.batch_normalization(h)
                 h = tf.nn.relu(h)
 
             elif self.n_layer == 2:
@@ -65,22 +65,21 @@ class AE_CF(object):
                 h = tf.sparse.matmul(inputs, w) + b + tf.sparse.matmul(sides, s)
                 # else:
                 #     h = b + tf.sparse.matmul(sides, s)
-                # h = tf.layers.batch_normalization(h)
+                h = tf.layers.batch_normalization(h)
                 h = tf.nn.tanh(h)
 
             elif i == (self.n_layer-1):
                 h = tf.matmul(h ,w) + b + tf.sparse.matmul(sides, s)
-                # h = tf.layers.batch_normalization(h)
+                h = tf.layers.batch_normalization(h)
                 h = tf.nn.tanh(h)
 
             else:
                 h = tf.matmul(h, w) + b + tf.sparse.matmul(sides, s)
-                # h = tf.layers.batch_normalization(h)
+                h = tf.layers.batch_normalization(h)
                 h = tf.nn.relu(h)
 
             prev_dim = h.get_shape()[1]
-
-        h = tf.layers.batch_normalization(h)
+        
         with tf.variable_scope('layer%d'%self.n_layer):
             w = tf.get_variable('weight', shape=[h.get_shape()[1], self.dims[-1]],
                                 trainable=True,
