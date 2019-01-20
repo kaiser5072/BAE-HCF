b48a9a5257daec5ffb564384cc2cc1d44c504a19
이기원 <kaiser5072@gmail.com>
2019-01-20 19:12:57 +0900

    Initial commit

--- model.py
+++ model.py
@@ -100,9 +100,9 @@ class AE_CF(object):
         # pref_diff_zero = tf.reduce_sum(tf.square(self.outputs)) - tf.reduce_sum(tf.square(self.preds))
         pred_top_k, _ = tf.nn.top_k(self.outputs, k=100)
         pref_diff_zero = tf.reduce_mean(tf.square(pred_top_k))
-        pref_diff_ones = tf.reduce_mean(tf.square(self.preds - 1))
+        # pref_diff_ones = tf.reduce_mean(tf.square(self.preds - 1))
 
-        self.loss = tf.add_n([pref_diff_ones, pref_diff_zero])
+        self.loss = tf.add_n([pref_diff_zero])
         self.loss = tf.identity(self.loss, name='loss')
 
         all_var = [var for var in tf.trainable_variables() ]
