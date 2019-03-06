7434a3d63af6e83ad7f59dbef9e4beed6277f1b4
이기원 <kaiser5072@gmail.com>
2019-03-07 00:47:16 +0900

    CiteULike

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+# TRAINING
 CUDA_VISIBLE_DEVICES=0 python main.py --dims 500 200 --data_dir 'Input/tmp' -b 512 -e 1400 --display_every 4000 --l2_lambda 0.00004 -r 0.0 -f 0
+CUDA_VISIBLE_DEVICES=0 python main.py --dims 500 200 --data_dir 'Input/tmp' -b 512 -e 1400 --display_every 4000 --l2_lambda 0.00004 -r 0.0 -f 1
+
+
+# EVALUATION
+CUDA_VISIBLE_DEVICES=0 python main.py --dims 500 200 --data_dir 'Input/tmp' -b 512 -e 1400 --display_every 4000 --l2_lambda 0.00004 -r 0.0 -f 0 -m predict
+CUDA_VISIBLE_DEVICES=0 python main.py --dims 500 200 --data_dir 'Input/tmp' -b 512 -e 1400 --display_every 4000 --l2_lambda 0.00004 -r 0.0 -f 1 -m predict
