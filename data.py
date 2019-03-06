3a61f19aa9838a6bad770add2e8534895d7a81e4
이기원 <kaiser5072@gmail.com>
2019-03-06 16:35:14 +0900

    CiteULike

--- data.py
+++ data.py
@@ -25,7 +25,9 @@ def parse_data(inputs):
     num_train, num_val = 0, 0
 
     sparse_tr = csr_matrix((pref_tr, (row_tr, col_tr)), shape=(height, width))
+    print('a')
     sparse_te = csr_matrix((pref_te, (row_te, col_te)), shape=(height, width))
+    print('b')
     sparse_co = csr_matrix((contents, (item, feature)), shape=(height, n_contents))
 
     with tqdm.tqdm(total=end-begin) as pbar:
