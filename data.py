a68c3f613acf1a1a146b959f4f183cac3a4cad4b
이기원 <kaiser5072@gmail.com>
2019-03-27 18:54:26 +0900

    Initial Commit

--- data.py
+++ data.py
@@ -186,8 +186,9 @@ class Data(object):
         pref = coo_matrix((rating, (row, column)), shape=(self.height, self.width))
         
         cut_list = pref.sum(axis=0)
+        print(cut_list)
         cut_idx = np.where(cut_list < 5)[0]
-        print(cut_idx)
+
         item_idx = np.setdiff1d(range(self.width), cut_idx)
         
         divider = np.random.uniform(0, 1, [self.height, np.size(item_idx, 0)])
